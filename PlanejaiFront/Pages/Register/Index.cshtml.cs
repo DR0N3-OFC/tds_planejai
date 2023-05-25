6deec8bb60ac1c6b1cf01988203a654691d239ae
Bruno Raphael Facundo <dronecraftbr10@gmail.com>
2023-05-25 13:35:51 -0300

    First Iteration: Adding Landing Page and Header Nav

---
+++ PlanejaiFront/Pages/Register/Index.cshtml.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PlanejaiFront.Pages.Register
+{
+    public class Index : PageModel
+    {
+        public Index()
+        {
+            
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
