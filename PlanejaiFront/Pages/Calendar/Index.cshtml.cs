a478cf3473f21b7372cde200c785cfea147720e0
Bruno Raphael Facundo <dronecraftbr10@gmail.com>
2023-06-05 13:25:41 -0300

    Trying to make this calendar work. AAAAAAAAAA
    Deus, me ajuda!

--- PlanejaiFront/Pages/Calendar/Index.cshtml.cs
+++ PlanejaiFront/Pages/Calendar/Index.cshtml.cs
@@ -3,10 +3,42 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace PlanejaiFront.Pages.Calendar
 {
-    public class IndexModel : PageModel
+    public class Index : PageModel
     {
-        public void OnGet()
+        [BindProperty]
+        public int Month { get; set; }
+        [BindProperty]
+        public int Year { get; set; }
+
+        [HttpGet]
+        public void OnGet(int month, int year)
         {
+            if (month != 0 && year != 0)
+            {
+                Month = month;
+                Year = year;
+            }
+        }
+
+        [HttpPost]
+        public IActionResult OnPostChangeMonth(string monthOffset)
+        {
+            if (monthOffset == "<")
+            {
+                if (Month! == 1)
+                {
+                    return Redirect($"/Calendar/Index/12/{Year - 1}");
+                }
+                return Redirect($"/Calendar/Index/{Month - 1}/{Year}");
+            }
+            else
+            {
+                if (Month == 12)
+                {
+                    return Redirect($"/Calendar/Index/1/{Year + 1}");
+                }
+                return Redirect($"/Calendar/Index/{Month + 1}/{Year}");
+            }
         }
     }
 }
